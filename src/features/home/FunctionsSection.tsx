48ad79e3120e79b056bef1cffc368e76aad9ade7
thanhmv <thanhmv@ahamove.com>
2022-08-25 23:46:46 +0700

    update overview

--- src/features/home/FunctionsSection.tsx
+++ src/features/home/FunctionsSection.tsx
@@ -1,21 +1,60 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+import Masonry from '@mui/lab/Masonry';
 
 const FunctionsSection = () => {
   const listServices = [
     {
-      title: 'ABC',
+      title: 'Custom Software Development',
+      contents: [
+        'Custom software solutions',
+        'Web and mobile applications development',
+        'Domain expertise',
+        'Large-scaled technical stack',
+      ],
     },
     {
-      title: 'ABC',
+      title: 'BI & Big Data Management',
+      contents: [
+        'Big Data Analytics',
+        'Data Warehouses for enhanced ETL operations',
+        'Real-time reports of your business operations',
+        'Data management costs optimization',
+      ],
     },
     {
-      title: 'ABC',
+      title: 'Software Re-engineering',
+      contents: [
+        'Legacy Software modernization ',
+        'Third-party data integration',
+        'Functionality Expanding',
+        'Infrastructure Setup',
+      ],
     },
     {
-      title: 'ABC',
+      title: 'Team Augmentation',
+      contents: [
+        'Team members with the right skills and experience',
+        'Temporary shortage or long-time engagement',
+        'Team ramp-up',
+      ],
     },
     {
-      title: 'ABC',
+      title: 'SaaS Development',
+      contents: [
+        'Building a Saas product from scratch',
+        'Tweaking an existing SaaS solution',
+        'Successful app transormation into SaaS',
+        'Expanding a localized SaaS app reach in new market',
+      ],
+    },
+    {
+      title: 'IT Consulting and Digital advisory',
+      contents: [
+        'Select the right technology for your business goal',
+        'Validate your product idea before investing in it',
+        'Attract investment with an elaborate product prototype',
+        'Add scalability and flexibility to your business',
+      ],
     },
   ];
   return (
@@ -23,20 +62,38 @@ const FunctionsSection = () => {
       <Typography variant='h2' gutterBottom>
         Our services
       </Typography>
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-        {listServices.map((item) => {
+      <Masonry columns={3} spacing={2}>
+        {listServices.map((item, index) => {
           return (
-            <Grid
-              item
-              xs={6}
-              md={4}
-              sx={{ backgroundColor: '#1F1F1F', borderRadius: 16 }}
-            >
-              <Box>{item.title}</Box>
-            </Grid>
+            <Card key={index}>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color='text.secondary'
+                  gutterBottom
+                  component='h6'
+                >
+                  {item.title}
+                </Typography>
+                <Box>
+                  {item.contents.map((text) => {
+                    return (
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color='text.secondary'
+                        gutterBottom
+                        component='h6'
+                      >
+                        - {text}
+                      </Typography>
+                    );
+                  })}
+                </Box>
+              </CardContent>
+            </Card>
           );
         })}
-      </Grid>
+      </Masonry>
     </Box>
   );
 };
